37d340abc1bcc0df38e7cf35675aa72eb9ddc1e6
Arsham <arsham.fadaie@gmail.com>
2024-04-25 17:43:18 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+using recursiveProgrammingSample.UnivercitySample.subtractionHumanAproach;
+
+int[,] array = new int[2,5]{  { 1, 2, 3, 4, 5 } , //first number
+                            { 0, 0, 7, 8, 9 }   //secound number
+                          }; //true result is 1 1 5 5 6
+
+/////recursive implement test
+IsubtractionHumanAproach objRecursive = new subtractionRecursive();
+
+var resultRecursive = objRecursive.Calculate(array);
+
+Console.WriteLine("\nrecursive output: ");
+
+foreach (var item in resultRecursive)
+{
+    Console.Write(item + " ");
+}
+
+/////simple implement test
+IsubtractionHumanAproach objsimple = new subtractionRecursive();
+var resultSimple = objsimple.Calculate(array);
+Console.WriteLine("\nSimple output: ");
+
+foreach (var item in resultSimple)
+{
+    Console.Write(item + " ");
+}
---
+++ UnivercitySample/subtractionHumanAproach/IsubtractionHumanAproach.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursiveProgrammingSample.UnivercitySample.subtractionHumanAproach
+{
+    public interface IsubtractionHumanAproach
+    {
+        public int[] Calculate(int[,] array);
+    }
+}
